9be9dce1c8c31dd842657e987c993b36eb0329f4
codlab <codlabtech@gmail.com>
2020-03-16 01:12:05 +0100

    prevent ignored files and use action

--- action.h
+++ action.h
@@ -1,6 +1,10 @@
 #ifndef ACTION_INCLUDED
 #define ACTION_INCLUDED
 
+#ifndef nullptr
+#define nullptr 0
+#endif
+
 #include "Joystick.h"
 
 void report_action(USB_JoystickReport_Input_t* const ReportData, Command* command);
