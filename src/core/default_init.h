e33dc359bc701fd9f35872f995007087be627e0b
codlab <codlabtech@gmail.com>
2020-03-29 14:40:46 +0200

    add possbility for using pre init controller discovery

---
+++ src/core/default_init.h
@@ -0,0 +1,33 @@
+/*
+    Pokemon Sword/Shield bots using Arduino
+    Copyright (C) 2020  brianuuuSonic / codlab
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+	Contextually based on the LUFA library's Low-Level Joystick Demo
+		(C) Dean Camera
+	Contextually based on the HORI's Pokken Tournament Pro Pad design
+		(C) HORI
+
+	For more information, please see the README.md available at https://github.com/codlab/arduino_farm_swsh
+*/
+
+#ifndef DEFAULT_INIT_INCLUDED
+#define DEFAULT_INIT_INCLUDED
+
+#include "action.h"
+
+Command* manage_init(Context* context);
+#endif
